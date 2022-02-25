ff4d2240cc4e2929d40090413ec0b85124e83bc6
ysaad01 <ysaad2325@gmail.com>
2022-02-25 10:58:12 -0500

    pseudocode and password character data added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,32 @@
-// Assignment code here
+// 1. prompt user for the password criteria
+//    - password length 8 - 128
+//    - lowercase, uppercase, numbers, special characters
+// 2. validate input 
+// 3. generate password based on criteria
+// 4. display it in textbox 
+
+// variables
+var pwLength;
+
+var lowerCaseLetter = 'abcdefghijklmnopqrstuvwxyz';
+var upperCaseLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var number = '1234567890';
+var specialCharacter = '~!@#$%^&*().';
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+function generatePassword() {
+  pwLength = parseInt(prompt('How long do you want to make your password? Choose between 8 - 128 characters'));
+};
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +34,5 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
