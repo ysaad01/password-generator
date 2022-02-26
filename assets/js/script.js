879a37b5da6dff2d49321adb404baf79ce72f7b0
ysaad01 <ysaad2325@gmail.com>
2022-02-26 18:17:01 -0500

    pw length prompt added to generatePassword function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,11 @@
 // 3. generate password based on criteria
 // 4. display it in textbox 
 
-// variables
-var pwLength;
-
-var lowerCaseLetter = 'abcdefghijklmnopqrstuvwxyz';
-var upperCaseLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var number = '1234567890';
-var specialCharacter = '~!@#$%^&*().';
-
+// characters
+var lowerCaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacter = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "."];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -20,10 +17,23 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
+// generatePassword function 
 function generatePassword() {
-  pwLength = parseInt(prompt('How long do you want to make your password? Choose between 8 - 128 characters'));
+  console.log("clicked button");
+  var newPwLength = prompt('How long do you want to make your password? Choose between 8 - 128 characters');
+  newPwLength = parseInt(newPwLength);
+  console.log(newPwLength);
+
+  // prompt user to choose password length
+  if (!newPwLength) {
+    alert("Enter a valid number.");
+  } else if (parseInt(newPwLength) < 8) {
+    alert("That is not within the password limit. Please try again.");
+    generatePassword();
+  } else if (parseInt(newPwLength) > 128) {
+    alert("That is not within the password limit. Please try again.");
+    generatePassword();
+  }
 };
 
 
@@ -35,4 +45,6 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+
 
