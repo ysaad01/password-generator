efe39cda264f8e26c10dc8acf25a6d19c4ed032d
ysaad01 <ysaad2325@gmail.com>
2022-02-26 19:11:26 -0500

    else statement added to have user select password characters and if statement added if no selection was made

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@
 var lowerCaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacter = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "."];
+var specialCharacter = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ".", "<", ">", "+", "=", "-", "_", "[", "]", ":", ";", "?", "/"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -20,19 +20,36 @@ generateBtn.addEventListener("click", writePassword);
 // generatePassword function 
 function generatePassword() {
   console.log("clicked button");
-  var newPwLength = prompt('How long do you want to make your password? Choose between 8 - 128 characters');
+  var newPwLength = prompt('Please choose a password length between 8 - 128 characters.');
   newPwLength = parseInt(newPwLength);
   console.log(newPwLength);
 
   // prompt user to choose password length
   if (!newPwLength) {
     alert("Enter a valid number.");
+    return "Password length has not been set."
   } else if (parseInt(newPwLength) < 8) {
     alert("That is not within the password limit. Please try again.");
     generatePassword();
   } else if (parseInt(newPwLength) > 128) {
     alert("That is not within the password limit. Please try again.");
     generatePassword();
+  }
+  
+  // user entered valid character response
+  else {
+    var hasLower = confirm("Click OK to include lower case letters in your new password.");
+    var hasUpper = confirm("Click OK to include upper case letters in your new password.");
+    var hasNumber = confirm("Click OK to include numbers in your new password.");
+    var hasCharacter = confirm("Click OK to include special characters in your new password.");
+    var selectedOptions = [];
+    var finalPass = "";
+
+
+    if (!hasLower && !hasUpper && !hasNumber && !hasCharacter) {
+      alert("You must choose a selection. Please try again.");
+      return "No selection made. No password generated"
+    }
   }
 };
 
